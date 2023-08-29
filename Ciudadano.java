c4cde8b108100e1ca020636ef44c2a6456981fcf
Max <maximiliano.mino.s@mail.pucv.cl>
2023-08-29 12:13:07 -0400

    basico

---
+++ Ciudadano.java
@@ -0,0 +1,11 @@
+public class Ciudadano {
+    private int edad;
+    private String nombre;
+    public String Rut;
+    
+    public Ciudadano(int edad, String nombre, String Rut){
+        this.edad = edad;
+        this.nombre = nombre;
+        this.Rut = Rut;
+    }
+}
---
+++ main.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.ArrayList;
+
+
+
+
+public class main{
+    public static void main(String[] args) throws IOException{
+
+
+
+    }
+}
